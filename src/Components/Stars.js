e07d1934d110da226d1923516f276ea94b763da0
yhlong0 <yhlong2@gmail.com>
2018-05-15 22:42:11 -0500

    add heart

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -5,7 +5,7 @@ const Stars = (props) => {
 
     let stars = [];
     for (let i = 0; i < props.numberOfStars; i++) {
-        stars.push(<i key={i} className="fa fa-star"></i>)
+        stars.push(<i key={i} className="far fa-heart fa-3x"></i>)
     }
 
     return (
